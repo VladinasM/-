fc7617f6cecd62e3d063c71258b667159a1137eb
VladinasM <69033234+VladinasM@users.noreply.github.com>
2020-12-07 19:17:05 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,23 +1,32 @@
-from tkinter import *
+import threading
 import time
+from tkinter import *
+
+delay = 0.03
+def rect_move(event):
+    while True:
+        s = c.coords(circle)
+        while s[2] > 251:
+            c.move(circle, -2, 0)
+            s = c.coords(circle)
+            time.sleep(delay)
+        
+        while s[2] >= 251 and s[2] < 537 :
+            c.move(circle, 2, 0)
+            s = c.coords(circle)
+            time.sleep(delay)
+
+
 root = Tk()
- 
-canvas = Canvas(root, bg='white', height=800, width=800)
-canvas.pack()
- 
-a = canvas.create_oval((500, 371), (537, 408), fill='black')
-b = canvas.create_oval((214, 371), (251, 408), fill='red')
-c = canvas.create_rectangle((502, 374), (529, 400), fill='blue')
-def move():
-    k1 = 500 - 214
-    kx = -1
-    for i in range(33):
-        canvas.move(c,0.03*k1*kx, 0)
-        canvas.update()
-        time.sleep(0.03)
- 
-B = Button(root, text ="Движение к красному", command = move)
- 
-B.pack()
- 
+c = Canvas(root, width=800, height=800, bg="white")
+c.pack()
+
+a = c.create_oval(500, 371, 537, 408, fill='green')
+b = c.create_oval(214, 371, 251, 408, fill='red')
+circle = c.create_rectangle(502, 374, 529, 400, fill='blue')
+#button = Button(root, text="Start", command=rect_move)
+#startButton = c.create_window(300, 100, window=button)
+thread = threading.Thread(target=rect_move, name="Okno", args=[c])
+thread.start()
+
 root.mainloop()
